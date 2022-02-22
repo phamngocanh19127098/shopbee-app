a2c2e11b0c54ffddcdb3029b1ea9b0f356e86c0b
phamngocanh19127098 <npham4533@gmail.com>
2022-02-22 14:13:12 +0700

    add cart length

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
 import { useParams} from 'react-router-dom'
+import { addToCart } from '../actions/cartActions';
 export default function CartScreen(props) {
     const params = useParams();
     const productId = params.id;  
     const qty = params.qty||1;
-    
+    const dispatch = useDispatch();
+    useEffect(()=>{
+      if(productId){
+        dispatch(addToCart(productId,qty))
+      }
+    },[dispatch, productId,qty])
   return (
     <div>
         <h1>Cart Screen</h1>
