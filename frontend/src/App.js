0368b01b23d8bca85d4095b3703b3d9e064242a9
phamngocanh19127098 <npham4533@gmail.com>
2022-02-24 13:48:11 +0700

    design place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import { signout } from './actions/userActions';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import PaymentMethodScreen from './screens/paymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -65,6 +66,7 @@ function App() {
       <Route path="/register" element={<RegisterScreen/>}></Route>
       <Route path="/payment" element={<PaymentMethodScreen/>}></Route>
       <Route path="/cart" element={<CartScreen />}></Route>
+      <Route path="/placeorder" element={<PlaceOrderScreen />}></Route>
       </Routes>
       </main>
       <footer className="row center">All right reserved</footer>
